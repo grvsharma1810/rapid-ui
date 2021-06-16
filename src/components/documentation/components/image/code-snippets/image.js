f24a0d0a5fab570ce2df39b628bb204e2dc05d0a
grvsharma1810 <gs8280601@gmail.com>
2021-06-16 14:12:35 +0530

    fix: image

--- src/components/documentation/components/image/code-snippets/image.js
+++ src/components/documentation/components/image/code-snippets/image.js
@@ -1,4 +1,4 @@
-const image = `<img class="img" style="width:100px" src="http://placehold.it/100" alt=""/>
-<img class="img-round" style="width:100px" src="http://placehold.it/100" alt=""/>`
+const image = `<img class="img" style="width:100px" src="https://picsum.photos/100" alt=""/>
+<img class="img-round" style="width:100px" src="https://picsum.photos/100" alt=""/>`
 
 export default image;
